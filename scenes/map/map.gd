ba47dc112cd44d283a9d537f9244c47761ad244d
bruno.lopes <95176651+koddoki@users.noreply.github.com>
2024-12-08 21:45:43 -0300

    Map update

--- scenes/map/map.gd
+++ scenes/map/map.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+# Make sure this number isn't higher than the number of positions
+@export var current_position := 1
+
+@onready var positions := $UpperScreen/Positions
+
+
+func _on_blink_timer_timeout() -> void:
+	var position : Sprite2D = positions.get_child(current_position)
+	position.visible = !position.visible
